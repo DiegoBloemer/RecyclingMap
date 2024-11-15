6e9695a18c6d8dbf8b84a3837edfd2d558ae851b
Diego Nyland Bloemer <diegobloemer04@gmail.com>
2024-11-15 15:41:45 -0300

    Finaliza as Telas de Login, cadastro, iniciada a tela principal

--- frontend/js/olho-magico.js
+++ frontend/js/olho-magico.js
@@ -1,17 +1,35 @@
 const password = document.getElementById("password");
 const togglePassword = document.getElementById("togglePassword");
 
-togglePassoword.addEventListener("mousedown", function () {
+togglePassword.addEventListener("mousedown", function () {
     password.type = "text";
-    togglePassoword.src = "/frontend/assets/imgs/eye_icon.svg";
+    togglePassword.src = "/frontend/assets/imgs/eye_icon.svg";
 });
 
-togglePassoword.addEventListener("mouseup", function () {
+togglePassword.addEventListener("mouseup", function () {
     password.type = "password";
-    togglePassoword.src = "/frontend/assets/imgs/eye-off_icon.svg";
+    togglePassword.src = "/frontend/assets/imgs/eye-off_icon.svg";
 });
 
-togglePassoword.addEventListener("mouseleave", function () {
+togglePassword.addEventListener("mouseleave", function () {
     password.type = "password";
-    togglePassoword.src = "/frontend/assets/imgs/eye-off_icon.svg";
+    togglePassword.src = "/frontend/assets/imgs/eye-off_icon.svg";
+});
+
+const repeat_password = document.getElementById("repeat_password");
+const togglePassword_2 = document.getElementById("togglePassword_2");
+
+togglePassword_2.addEventListener("mousedown", function () {
+    repeat_password.type = "text";
+    togglePassword_2.src = "/frontend/assets/imgs/eye_icon.svg";
+});
+
+togglePassword_2.addEventListener("mouseup", function () {
+    repeat_password.type = "password";
+    togglePassword_2.src = "/frontend/assets/imgs/eye-off_icon.svg";
+});
+
+togglePassword_2.addEventListener("mouseleave", function () {
+    repeat_password.type = "password";
+    togglePassword_2.src = "/frontend/assets/imgs/eye-off_icon.svg";
 });
