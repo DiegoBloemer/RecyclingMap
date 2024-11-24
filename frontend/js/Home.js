766d3c8f3c5d038b316ce9d9ca1f544925ba12c6
Diego Nyland Bloemer <diegobloemer04@gmail.com>
2024-11-24 11:49:29 -0300

    Front concluído

--- frontend/js/Home.js
+++ frontend/js/Home.js
@@ -9,42 +9,46 @@ async function initMap() {
     zoom: 15,
     mapId: "MAP_DEMO_ID",
   });
+
+  const contato_internet = document.createElement("div");
+  contato_internet.className = "contato-internet";
+  contato_internet.textContent = "Contato Internet";
+
+const marker2 = new AdvancedMarkerElement({
+  map,
+  position: { lat: -28.953375003337133, lng: -49.50343503965838 },
+  content: contato_internet,
+});
+
+const forms = document.getElementById("localForm");
+
+forms.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(forms);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+
+  const newLocationForMap = document.createElement("div");
+
+  // Adiciona a classe ao elemento
+  newLocationForMap.classList.add("newLocationForMap");
+
+  // Define o conteúdo do elemento
+  newLocationForMap.textContent = "Nova localização";
+
+  // Aplica estilos diretamente via JavaScript
+  newLocationForMap.style.backgroundColor = "#4286f5";
+  newLocationForMap.style.borderRadius = "8px";
+  newLocationForMap.style.color = "#ffffff";
+  newLocationForMap.style.fontSize = "14px";
+  newLocationForMap.style.padding = "10px 15px";
+  newLocationForMap.style.position = "relative";
+
+  new AdvancedMarkerElement({
+    map,
+    position: { lat: -28.953863171992705, lng: -49.50189008724842 },
+    content: newLocationForMap,
+  });
+});
 }
-
-//   const forms = document.getElementById("form");
-
-//   forms.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const formData = new FormData(forms);
-//     const data = Object.fromEntries(formData.entries());
-//     console.log(data);
-    
-    
-    
-    /*
-    const newLocationForMap = document.createElement("div");
-
-    // Adiciona a classe ao elemento
-    newLocationForMap.classList.add("newLocationForMap");
-
-    // Define o conteúdo do elemento
-    newLocationForMap.textContent = "Nova localização";
-
-    // Aplica estilos diretamente via JavaScript
-    newLocationForMap.style.backgroundColor = "#4286f5";
-    newLocationForMap.style.borderRadius = "8px";
-    newLocationForMap.style.color = "#ffffff";
-    newLocationForMap.style.fontSize = "14px";
-    newLocationForMap.style.padding = "10px 15px";
-    newLocationForMap.style.position = "relative";
-
-    new AdvancedMarkerElement({
-      map,
-      position: { lat: -28.953863171992705, lng: -49.50189008724842 },
-      content: newLocationForMap,
-    });
-    */
-//   });
-// }
-
 initMap();
